f39038377fa42e32af8d2e7b73fe28f9fdd8b109
Crowdbotics <team@crowdbotics.com>
2020-05-28 05:39:24 +0000

    Update models

--- backend/liq_n_roll_16384/wsgi.py
+++ backend/liq_n_roll_16384/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liq_n_roll_16384.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liq_n_roll_16384.settings")
 
 application = get_wsgi_application()
